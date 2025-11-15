f0486159bcfe1d282ac74627111e9779479de059
deltamemories <maxenot123@gmail.com>
2025-11-16 00:16:59 +0200

    lab2: add pixel, char render (maybe unstable)

--- lab2/main.py
+++ lab2/main.py
@@ -1,12 +1,57 @@
 # lab2
-# example: ConsolePrinter.print(text: str, color: Color, position: Tuple[int, int], symbol: str)
+# example: ConsolePrinter.print_static(text: str, color: Color, position: Tuple[int, int], symbol: str)
+import json
+import time
 from enum import Enum
 
+
 class ConsolePrinter:
-	def __init__(self, path_to_font_config):
+	_FONT: dict[str, list[str]] = {}
+	_FONT_LOADED: bool = False
+
+	@staticmethod
+	def _load_font(file_path: str) -> None:
+		try:
+			with open(file_path, "r", encoding="utf-8") as f:
+				ConsolePrinter._FONT = json.load(f)
+				ConsolePrinter._FONT_LOADED = True
+		except FileNotFoundError as e:
+			pass # TODO
+		except json.JSONDecodeError as e:
+			pass # TODO
+		except Exception as e:
+			raise e
+
+	def __init__(self, path_to_font_config, color: AnsiColors, position: tuple[int, int], symbol: str):
+		if not ConsolePrinter._FONT_LOADED:
+			return # TODO TMP
+			ConsolePrinter._load_font(path_to_font_config)
+
+		self._path_to_font_config = path_to_font_config
+		self._color = color
+		self._position = position
+		self._symbol = symbol
+
+
+	@staticmethod
+	def draw_pixel(row: int, col: int, color: AnsiColors, symbol: str):
+		pos = f"\033[{row+1};{col+1}H"
+
+		print(f"{color.value}{pos}{symbol}{AnsiColors.RESET.value}", end='', flush=True)
+
+	@staticmethod
+	def draw_char(row: int, col: int, char_template: list[str], color: AnsiColors, symbol: str, ):
+		for i, line in enumerate(char_template):
+			for j, char in enumerate(line):
+				if char == "*":
+					ConsolePrinter.draw_pixel(row+i, col+j, color, symbol)
+
+
+	@classmethod
+	def print_static(cls):
 		pass
 
-	def print(self, text: str, color: AnsiColors, position: tuple[int, int]):
+	def print(self, text: str, color: AnsiColors, position: tuple[int, int], symbol: str):
 		pass
 
 
@@ -35,13 +80,14 @@ class AnsiColors(Enum):
 	BG_RED = "\033[41m"
 
 
+p = ConsolePrinter(None, None, None, None)
+print("\033[?1049h", end='', flush=True)
+
+p.draw_char(0, 0, ["__*__", "__*__", "_*_*_", "_*_*_", "*****", "*___*", "*___*"], AnsiColors.RED, symbol='*')
+p.draw_char(0, 7, ["***__", "*__*_", "***__", "*____", "****_", "*___*", "*****"], AnsiColors.RED, symbol='*')
+p.draw_char(0, 14, ["****_", "*____", "*____", "*____", "*____", "*____", "****_"], AnsiColors.RED, symbol='*')
 
 
-# Пример использования:
-
-print(f"{AnsiColors.RED.value}Это красный текст."
-			f"{AnsiColors.RESET.value} А это снова обычный текст по умолчанию.")
-
-print(f"{AnsiColors.BRIGHT_GREEN.value}Яркий зеленый текст."
-			f"{AnsiColors.RESET.value}")
+time.sleep(5)
+print("\033[?1049l", end='', flush=True)
 
