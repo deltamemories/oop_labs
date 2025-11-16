572b95546b907de8705ec31af89f4e44193c0113
deltamemories <maxenot123@gmail.com>
2025-11-16 16:19:02 +0200

    lab3: add main logic (maybe unstable)

--- lab3/log.py
+++ lab3/log.py
@@ -1,5 +1,7 @@
-from enum import Enum
+import datetime
+import re
 from abc import ABC, abstractmethod
+from enum import Enum
 
 
 class LogLevel(Enum):
@@ -14,13 +16,82 @@ class ILogFilter(ABC):
 	def match(self, log_level: LogLevel, text: str) -> bool:
 		pass
 
+class SimpleLogFilter(ILogFilter):
+	def __init__(self, pattern: str):
+		self._pattern = pattern
 
-class ILogFormatter(ABC):
-	pass
+	def match(self, log_level: LogLevel, text: str) -> bool:
+		return self._pattern in text
+
+class ReLLogFilter(ILogFilter):
+	def __init__(self, re_pattern: str):
+		self._re_pattern = re_pattern
+
+	def match(self, log_level: LogLevel, text: str) -> bool:
+		return re.match(self._re_pattern, text) is not None
+
+class LevelFilter(ILogFilter):
+	def __init__(self, level: LogLevel):
+		self._level = level
+
+	def match(self, log_level: LogLevel, text: str) -> bool:
+		return self._level.value == log_level.value
 
 
 class ILogHandler(ABC):
-	pass
+	@abstractmethod
+	def handle(self, log_level: LogLevel, text: str) -> None:
+		pass
+
+class FileLogHandler(ILogHandler):
+	def __init__(self, file_path: str):
+		self._file_path = file_path
+		self._file_descriptor = None
+		try:
+			f = open(self._file_path, "a")
+		except FileNotFoundError as e:
+			raise e
+		except PermissionError as e:
+			raise e
+		except Exception as e:
+			raise e
+		else:
+			self._file_descriptor = f
+
+	def __del__(self):
+		if self._file_descriptor is not None:
+			self._file_descriptor.close()
+
+	def handle(self, log_level: LogLevel, text: str) -> None:
+		print(f"{log_level.value}: {text}", file=self._file_descriptor)
+
+
+class SocketHandler(ILogHandler):
+	def handle(self, log_level: LogLevel, text: str) -> None:
+		pass # TODO SocketHandler.handle
+
+class ConsoleHandler(ILogHandler):
+	def handle(self, log_level: LogLevel, text: str) -> None:
+		print(f"{log_level.value}: {text}")
+
+class SysLogHandler(ILogHandler):
+	def handle(self, log_level: LogLevel, text: str) -> None:
+		pass # TODO SysLogHandler.handle
+
+class FtpHandler(ILogHandler):
+	def handle(self, log_level: LogLevel, text: str) -> None:
+		pass # TODO FtpHandler.handle
+
+
+class ILogFormatter(ABC):
+	@abstractmethod
+	def format(self, log_level: LogLevel, text: str) -> str:
+		pass
+
+
+class Formatter(ILogFormatter):
+	def format(self, log_level: LogLevel, text: str) -> str:
+		return f"[{log_level}] [{datetime.datetime.now()}] {text}" # TODO make in data:yyyy.MM.dd hh:mm:ss format
 
 
 class Logger:
@@ -30,16 +101,25 @@ class Logger:
 			formatters: list[ILogFormatter],
 			handlers: list[ILogHandler]
 	):
-		pass
+		self._filters = filters
+		self._formatters = formatters
+		self._handlers = handlers
+
+	def _log(self, log_level: LogLevel, text: str) -> None:
+		if all([filter.match(log_level, text) for filter in self._filters]):
+			for formatter in self._formatters:
+				text = formatter.format(log_level, text)
+			for handler in self._handlers:
+				handler.handle(log_level, text)
 
 	def log(self, log_level: LogLevel, text: str) -> None:
 		pass
 
 	def log_info(self, text: str) -> None:
-		pass
+		self.log(LogLevel.INFO, text)
 
 	def log_warn(self, text: str) -> None:
-		pass
+		self.log(LogLevel.WARNING, text)
 
 	def log_error(self, text: str) -> None:
-		pass
+		self.log(LogLevel.ERROR, text)
