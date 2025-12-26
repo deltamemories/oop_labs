33498f58545199e9ffaf0de9c5e254c468ac3e33
Delta <84548466+deltamemories@users.noreply.github.com>
2025-12-26 14:53:27 +0200

    lab1: fix

--- lab1/main.ts
+++ lab1/main.ts
@@ -286,7 +286,7 @@ class AngleRange {
 				return this._endInclusive;
 			}
 
-			if (this.start < this.end) {
+			if (this.start.radians < this.end.radians) {
 				return other.radians > this.start.radians && other.radians < this.end.radians;
 			} else {
 				return other.radians < this.start.radians && other.radians > this.end.radians;
